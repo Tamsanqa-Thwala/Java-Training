0393a1d13352df899696c48cf16e5db5371dc891
Tamsanqa Thwala <Tamsanqa.Thwala@ilabqa.com>
2025-01-09 15:43:54 +0200

    Fix errors in debug exercises

---
+++ Java-A-Training/src/Chapter6/DebugExercise4.java
@@ -0,0 +1,9 @@
+package Chapter6;
+public class DebugExercise4 {
+   public static void main(String[] args) {         
+  int x = 5;         
+  int y = 2;         
+  double result = (double)x / y;
+  System.out.println("Result: " + result);     
+  } 
+}
